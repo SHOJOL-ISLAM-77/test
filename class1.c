ec831d07e476333a8687553db2edd52fb43d7a2e
Shojol <shojolislam.webappick@gmail.com>
2025-01-16 17:45:13 +0600

    first commit

---
+++ class1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  return 0;
+}
+
+// average of three numbers;
+
+/*
+{
+
+  int a, b, c;
+  printf("Inter first number :");
+  scanf("%d", &a);
+  printf("Inter second number :");
+  scanf("%d", &b);
+  printf("Inter third number : ");
+  scanf("%d", &c);
+  int average = (a + b + c) / 3;
+
+  printf("Average of %d , %d , %d, is: %d \n", a, b, c, average);
+}
+*/
+
+// data types
+
+/* {
+  int number = 77;
+  int age = 18;
+  float pi = 3.1416;
+  char my_name[] = "Shojol Islam";
+  char star = '*';
+
+  printf("Hello, my name is %s \n", my_name);
+  printf("I'm %d years old \n", age);
+  printf("The %s is %c \n", "star", star);
+  printf("The value of pi is approximately %f \n", pi);
+  return 0;
+}
+*/
